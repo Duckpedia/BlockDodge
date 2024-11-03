e5a8197d70efd322f5c0b224068c582c09253da0
Nika Hočevar <nika.hocevar67@gmail.com>
2024-11-03 20:23:39 +0100

    Changed the warning on android that it isn't compatible. Highest android version now 14.0, lowest 5.1. Optimized the update functions a bit (fixedupdate)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -26,7 +26,7 @@ public class Player : MonoBehaviour
             }
         }
         else{
-            rb.velocity = Vector2.zero;
+            rb.linearVelocity = Vector2.zero;
         }
 
     }
